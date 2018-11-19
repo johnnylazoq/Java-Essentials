3b863110c4129ddf95f28fd4ebfdc5d80d619930
johnn <johnn@192.168.10.146>
2018-11-19 23:55:22 +0100

    df

--- Polymorphism/src/OOP/Company.java
+++ Polymorphism/src/OOP/Company.java
@@ -5,9 +5,9 @@ public class Company {
 	public static void main(String[] args) {
 		// Company personell
 		Staff personnel = new Staff();
-		// testsd
+		// testsds
 		personnel.payday();
-
+//hfh
 	}
 
 }
