9ba53c69b9b0d1fb0d944ecceec25f4fd290751d
johnn <johnn@192.168.10.146>
2018-11-19 23:10:25 +0100

    Polymorphism

---
+++ Polymorphism/src/OOP/Company.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class Company {
+
+	public static void main(String[] args) {
+		
+		Staff personnel = new Staff();
+		
+		personnel.payday();
+
+	}
+
+}
---
+++ Polymorphism/src/OOP/Volunteer.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Volunteer extends StaffMember 
+{
+	
+	public Volunteer (String eName, String eAddress, String ePhone) 
+	{
+		super(eName, eAddress, ePhone);
+		
+	}
+	
+	public double pay()
+	{
+		return 0.0;
+	}
+}
