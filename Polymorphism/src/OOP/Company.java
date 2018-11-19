168a8e2547754081faf2be55f84ce668b0940e0a
johnn <johnn@192.168.10.146>
2018-11-19 23:25:12 +0100

    ddd

--- Polymorphism/src/OOP/Company.java
+++ Polymorphism/src/OOP/Company.java
@@ -3,7 +3,7 @@ package OOP;
 public class Company {
 
 	public static void main(String[] args) {
-		// Company personel
+		// Company personell
 		Staff personnel = new Staff();
 		
 		personnel.payday();
